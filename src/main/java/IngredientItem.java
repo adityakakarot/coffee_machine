6e638671b8396529ba2335a32a4b11929f2d6c4e
Aditya Sharma <adityasharma.code@gmail.com>
2020-08-05 08:12:09 +0530

    added ingredient item and beverage model

---
+++ src/main/java/IngredientItem.java
@@ -0,0 +1,10 @@
+public class IngredientItem {
+  private Ingredient ingredient;
+
+  private Integer quantity;
+
+  public IngredientItem(Ingredient ingredient, Integer quantity) {
+    this.ingredient = ingredient;
+    this.quantity = quantity;
+  }
+}
