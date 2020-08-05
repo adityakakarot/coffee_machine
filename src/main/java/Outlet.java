e7999d884c037df88f2e132037ba8abd10895485
Aditya Sharma <adityasharma.code@gmail.com>
2020-08-05 08:12:22 +0530

    added outlet and coffee machine model

---
+++ src/main/java/Outlet.java
@@ -0,0 +1,15 @@
+public class Outlet {
+  private Boolean isAvailable;
+
+  public Outlet() {
+    isAvailable = true;
+  }
+
+  public Boolean getAvailable() {
+    return isAvailable;
+  }
+
+  public void setAvailable(Boolean available) {
+    isAvailable = available;
+  }
+}
