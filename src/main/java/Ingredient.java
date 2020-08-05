274b6bd7fb9be0bd9c90ad8384348c2a9d3d16c9
Aditya Sharma <adityasharma.code@gmail.com>
2020-08-05 08:11:57 +0530

    added ingredients model

---
+++ src/main/java/Ingredient.java
@@ -0,0 +1,11 @@
+public class Ingredient {
+  private final String identifier;
+
+  public Ingredient(String identifier) {
+    this.identifier = identifier;
+  }
+
+  public String getIdentifier() {
+    return identifier;
+  }
+}
